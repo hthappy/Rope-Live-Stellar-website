e08953f5a0ee05b8612b8a0901046afbe5813683
hthappy <tivon.he@qq.com>
2025-06-17 02:15:03 +0800

    update

---
+++ website/openwrt/router_init.sh
@@ -0,0 +1,45 @@
+# 初始化脚本# 定义下载链接和目标目录
+URL1="https://stellar.ai-yy.com/openwrt/footer.htm"
+URL2="https://stellar.ai-yy.com/openwrt/footer_login.htm"
+URL3="https://stellar.ai-yy.com/openwrt/sysauth.htm"
+LOGO="https://stellar.ai-yy.com/openwrt/logo.png"
+TARGET_DIR="/usr/lib/lua/luci/view/themes/argon/"
+
+# 检查目标目录是否存在
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "错误：目标目录 $TARGET_DIR 不存在！"
+    exit 1
+fi
+
+# 下载logo
+echo "正在下载 logo.png..."
+wget -q --no-check-certificate "$LOGO" -O "/www/luci-static/argon/img/logo.png"
+if [ $? -ne 0 ]; then
+    echo "下载 logo.png 失败！"
+    exit 1
+fi
+
+# 下载文件并覆盖
+echo "正在下载 footer.htm..."
+wget -q --no-check-certificate "$URL1" -O "$TARGET_DIR/footer.htm"
+if [ $? -ne 0 ]; then
+    echo "下载 footer.htm 失败！"
+    exit 1
+fi
+
+echo "正在下载 footer_login.htm..."
+wget -q --no-check-certificate "$URL2" -O "$TARGET_DIR/footer_login.htm"
+if [ $? -ne 0 ]; then
+    echo "下载 footer_login.htm 失败！"
+    exit 1
+fi
+
+echo "正在下载 sysauth.htm..."
+wget -q --no-check-certificate "$URL3" -O "$TARGET_DIR/sysauth.htm"
+if [ $? -ne 0 ]; then
+    echo "下载 sysauth.htm 失败！"
+    exit 1
+fi
+
+
+/etc/init.d/uhttpd restart
